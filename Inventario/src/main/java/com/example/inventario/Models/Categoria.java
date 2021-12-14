a2b1c20d9e388f2466f78aeb965151149d571153
DeePBlack82 <dapabai@gmail.com>
2021-12-14 14:21:41 +0100

    Creación clases "Producto" y "Categoria"
    Modificación del "Application.Properties"

---
+++ Inventario/src/main/java/com/example/inventario/Models/Categoria.java
@@ -0,0 +1,65 @@
+package com.example.inventario.Models;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Categorias")
+public class Categoria {
+
+	@Id
+    @Column(name="Id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name="Nombre", nullable = false)
+    private String nombre;
+    
+    @ManyToMany(mappedBy = "categorias")
+    private List<Producto> productos;
+
+    
+    
+	public Categoria() {
+		super();
+	}
+
+	public Categoria(String nombre, List<Producto> productos) {
+		super();
+		this.nombre = nombre;
+		this.productos = productos;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nombre=" + nombre + "]";
+	}
+    
+    
+    
+}
---
+++ Inventario/src/main/java/com/example/inventario/Models/Producto.java
@@ -0,0 +1,71 @@
+package com.example.inventario.Models;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Productos")
+public class Producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Id", unique = true, nullable = false)
+	private Long id;
+	
+	@Column(name = "Nombre", nullable = false)
+	private String nombre;
+	
+	@Column(name = "Precio", nullable = false)
+	private Double precio;
+	
+	@JoinTable(name = "Productos_Categorias",
+	        joinColumns = @JoinColumn(name = "Id_Producto", referencedColumnName = "Id", nullable = false),
+	        inverseJoinColumns = @JoinColumn(name="Id_Categoria", referencedColumnName = "Id", nullable = false))
+	@ManyToMany
+	private List<Categoria> categorias;
+
+	
+	
+	public Producto() {
+		super();
+	}
+
+	public Producto(String nombre, List<Categoria> categorias) {
+		super();
+		this.nombre = nombre;
+		this.categorias = categorias;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", categorias=" + categorias + "]";
+	}
+
+	
+	
+}
